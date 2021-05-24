d4c0336fea018536073720ac3bbd7dee217c9d12
als10 <alston03.dmello@gmail.com>
2021-05-24 15:23:33 +0400

    uibuild

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, us
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(middleware.tokenExtractor)
 
 app.use('/api/projects', projectsRouter)
